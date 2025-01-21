7980925bd1d5e4d34928f219f6985fd8baf30a8c
Almario Miano <almariomiano@gmail.com>
2025-01-21 10:33:22 +0800

    color and font setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto'],
+      },
+      colors: {
+        flax: '#EAD985',
+        'metallic-seaweed': '#007990',
+        'space-cadet': '#222E50',
+        zomp: '#439A86',
+        'jet-stream': '#BCD8C1',
+      },
+    },
   },
   plugins: [],
 };
